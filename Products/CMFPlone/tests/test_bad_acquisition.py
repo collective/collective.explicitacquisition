1209964170742e50d9f67882081fe5f16d0aec3b
Godefroid Chapelle <gotcha@bubblenet.be>
2013-11-20 12:03:08 +0100

    NotFound is also raised when accessing template

--- Products/CMFPlone/tests/test_bad_acquisition.py
+++ Products/CMFPlone/tests/test_bad_acquisition.py
@@ -33,3 +33,24 @@ class TestBadAcquisition(unittest.TestCase):
             error is not None,
             msg='Acquired content should not be published.')
         self.assertEqual(404, error.code)
+
+    def test_not_found_when_template_on_acquired_content(self):
+        "browsing to template on acquired content should trigger a 404"
+        self.portal.invokeFactory('Document', 'a_page')
+        self.assertTrue('a_page' in self.portal.objectIds())
+        self.portal.invokeFactory('Folder', 'a_folder')
+        self.assertTrue('a_folder' in self.portal.objectIds())
+        import transaction
+        transaction.commit()
+        browser = Browser(self.app)
+        browser.open(self.portal.a_page.absolute_url())
+        error = None
+        try:
+            url = self.portal.absolute_url() + '/a_folder/a_page/document_view'
+            browser.open(url)
+        except HTTPError, e:
+            error = e
+        self.assertTrue(
+            error is not None,
+            msg='Acquired content should not be published.')
+        self.assertEqual(404, error.code)
